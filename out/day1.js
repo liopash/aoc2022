e3b7f766c8bbaba9c015758577732bea98a61e2f
Tomas Barcak <tomas.barcak@vestberry.com>
2022-12-03 14:02:58 +0100

    remove out files

--- out/day1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const aocHelper_1 = require("./aocHelper");
-const input = (_a = (0, aocHelper_1.inputReader)('./day1.txt')) !== null && _a !== void 0 ? _a : '';
-console.log((0, aocHelper_1.parseInput)(input));
