e73af3b386d9f799e925050d058f8507ed377636
Tomas Barcak <tomas.barcak@vestberry.com>
2022-12-05 09:18:13 +0100

    refactor

--- src/day5.ts
+++ src/day5.ts
@@ -14,7 +14,7 @@ const input = inputReader('./day5.txt')
 //  1   2   3   4   5   6   7   8   9
 // `
 
-const initialCratesP1 = [
+const initialCrates = () => ([
     [],
     [...'NBDTVGZJ'],
     [...'SRMDWPF'],
@@ -25,68 +25,45 @@ const initialCratesP1 = [
     [...'GCVBPQ'],
     [...'ZBPN'],
     [...'WPJ'],
-]
-
-const initialCratesP2 = [
-    [],
-    [...'NBDTVGZJ'],
-    [...'SRMDWPF'],
-    [...'VCRSZ'],
-    [...'RTJZPHG'],
-    [...'TCJNDZQF'],
-    [...'NVPWGSFM'],
-    [...'GCVBPQ'],
-    [...'ZBPN'],
-    [...'WPJ'],
-]
+])
 
 const crateOperator9000 = (input: number[], crates: string[][]) => {
     let [amount, from, to] = input
-    const fromRow = crates[from]
-    const toRow = crates[to]
     while (amount > 0) {
-        toRow.push(fromRow.pop() as string)
+        crates[to].push(crates[from].pop() as string)
         amount--
     }
 }
 
 const crateOperator9001 = (input: number[], crates: string[][]) => {
     let [amount, from, to] = input
-    const fromRow = crates[from]
-    const toRow = crates[to]
-    fromRow.splice(fromRow.length - amount, amount).forEach(item => toRow.push(item))
+    crates[from]
+        .splice(crates[from].length - amount, amount)
+        .forEach(item => crates[to].push(item))
+}
+
+const elvesWork = (input: string, initial: string[][], fn: (input: number[], crates: string[][]) => void) => {
+    const parsed = (parseInput(input) as string[][]).map(item => item
+        .map((item: string) => isNaN(parseInt(item)) ? null : parseInt(item))
+        .filter(Boolean)
+    )
+    parsed.forEach(input => {
+        fn(input as number[], initial)
+    })
+    initial.shift()
+
+    return initial.reduce((curr, row) => {
+        return curr + row[row.length - 1]
+    }, '')
 }
 
 export const day5 = (input: string, initial: string[][]) => {
-    const parsed = (parseInput(input) as string[][]).map(item => item
-        .map((item: string) => isNaN(parseInt(item)) ? null : parseInt(item))
-        .filter(Boolean)
-    )
-    parsed.forEach(input => {
-        crateOperator9000(input as number[], initial)
-    })
-    initial.shift()
-
-    return initial.reduce((curr, row) => {
-        return curr + row[row.length - 1]
-    }, '')
+    return elvesWork(input, initial, crateOperator9000)
 }
 
 export const day52 = (input: string, initial: string[][]) => {
-    const parsed = (parseInput(input) as string[][]).map(item => item
-        .map((item: string) => isNaN(parseInt(item)) ? null : parseInt(item))
-        .filter(Boolean)
-    )
-
-    parsed.forEach(input => {
-        crateOperator9001(input as number[], initial)
-    })
-    initial.shift()
-
-    return initial.reduce((curr, row) => {
-        return curr + row[row.length - 1]
-    }, '')
+    return elvesWork(input, initial, crateOperator9001)
 }
 
-console.log('day5 part1', day5(input, initialCratesP1))
-console.log('day5 part2', day52(input, initialCratesP2))
+console.log('day5 part1', day5(input, initialCrates()))
+console.log('day5 part2', day52(input, initialCrates()))
